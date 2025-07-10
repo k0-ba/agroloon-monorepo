ad905d8fa5b7721d41b15dbf78ff85277f11ec1e
KertészDomokos <kerteszdomokos@gmail.com>
2025-07-10 13:23:51 +0200

    risky: added apollo tracking to generate leads

--- apps/website/app/layout.tsx
+++ apps/website/app/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Analytics } from '@vercel/analytics/next';
 import { Header } from "@/components/header";
 import { Space_Mono } from "next/font/google";
+import Script from "next/script";
 
 import "./globals.css";
 
@@ -19,6 +20,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script id="apollo-tracker" strategy="afterInteractive">
+          {`
+            function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+            o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+            o.onload=function(){window.trackingFunctions.onLoad({appId:"686f9fceb47c1d0011f98726"})},
+            document.head.appendChild(o)}initApollo();
+          `}
+        </Script>
+      </head>
       <body className={`${spaceMono.variable} antialiased`}>
         <PosthogProvider>
           <ThemeProvider
